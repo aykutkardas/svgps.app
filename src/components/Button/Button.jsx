ee8668f98ca0aaa6659478a8b4c3e45ad78a70a7
gizemnkorkmaz <gizemnurkorkmaz@gmail.com>
2022-02-11 22:33:33 +0300

    Change Button to arrow function expression

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,12 +2,10 @@ import React from "react";
 
 import styles from "./Button.module.css";
 
-function Button({ children, ...props }) {
-  return (
-    <button className={styles.Button} {...props}>
-      {children}
-    </button>
-  );
-}
+const Button = ({ children, ...props }) => (
+  <button className={styles.Button} {...props}>
+    {children}
+  </button>
+);
 
 export default Button;
