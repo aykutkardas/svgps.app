9bed6101ae19b1576bf954fb03b560a483575741
gizemnkorkmaz <gizemnurkorkmaz@gmail.com>
2022-02-12 15:37:50 +0300

    Change import order

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,5 @@
-import React from "react";
-
-import styles from "./Header.module.css";
 import Icon from "../Icon";
+import styles from "./Header.module.css";
 
 const Header = () => (
   <div className={styles.Header}>
