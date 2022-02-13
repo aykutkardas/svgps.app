41d91c9cb1b0f77729a081477a49d7d47632e415
gizemnkorkmaz <gizemnurkorkmaz@gmail.com>
2022-02-13 14:26:20 +0300

    Remove forceUpdate and updateState from App

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import "react-toastify/dist/ReactToastify.css";
 
-import { useCallback, useState } from "react";
+import { useState } from "react";
 import { ToastContainer } from "react-toastify";
 import Header from "./components/Header/Header";
 import DropZoneWrapper from "./components/DropZoneWrapper/DropZoneWrapper";
@@ -10,8 +10,6 @@ import IconPreviewArea from "./components/IconPreviewArea";
 
 export default function App() {
   const [icons, setIcons] = useState([]);
-  const [, updateState] = useState();
-  const forceUpdate = useCallback(() => updateState({}), []);
 
   return (
     <div className="App">
