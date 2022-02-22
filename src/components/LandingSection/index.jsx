36dcdbf206cd3c4ac2d73d5edbf0618db0bdf44e
Aykut Kardaş <aykutkrds@gmail.com>
2022-02-23 01:09:07 +0300

    Update LandingSection

--- src/components/LandingSection/index.jsx
+++ src/components/LandingSection/index.jsx
@@ -2,11 +2,10 @@ import styles from "./LandingSection.module.css";
 
 const LandingSection = () => (
   <div className={styles.LandingSection}>
-    <h1 className={styles.Header}>KISA AÇIKLAMA İÇEREN BİR BAŞLIK</h1>
+    <h1 className={styles.Header}>SVGPS</h1>
     <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur
-      eligendi veniam quo et saepe id enim fugiat maxime fuga animi. Eligendi,
-      quasi sunt saepe.
+      Online <span className={styles.Highlight}>selection.json</span> file
+      generator.
     </p>
   </div>
 );
