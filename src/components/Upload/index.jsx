f78a58e8b401b99b7c4a7146ee392dc9cadfa4a1
gizemnkorkmaz <gizemnurkorkmaz@gmail.com>
2022-02-27 12:10:46 +0300

    Add nanoid && Add id property to icons

--- src/components/Upload/index.jsx
+++ src/components/Upload/index.jsx
@@ -3,6 +3,7 @@ import styles from "./Upload.module.css";
 import { useRef } from "react";
 import { toast } from "react-toastify";
 import { getFormattedName, parse } from "svgps";
+import { nanoid } from "nanoid";
 import Button from "../Button";
 
 const Upload = ({ icons, setIcons }) => {
@@ -10,6 +11,7 @@ const Upload = ({ icons, setIcons }) => {
 
   const handleFileInput = async (event) => {
     const selectedIcons = [];
+
     if (window.FileList && window.File && window.FileReader) {
       for (let i = 0; i < event.target.files.length; i++) {
         const file = event.target.files[i];
@@ -30,8 +32,8 @@ const Upload = ({ icons, setIcons }) => {
         const content = await blob.text();
 
         selectedIcons.push({
+          id: nanoid(),
           name: getFormattedName(file.name),
-          fileName: file.name,
           content,
           ...parse(content),
         });
