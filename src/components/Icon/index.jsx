19abaa92f246510f7aac9e8d0e5b4a125dc58754
Aykut Kardaş <aykutkrds@gmail.com>
2022-05-11 21:19:26 +0300

    Use `selection.json` file from SVGPS

--- src/components/Icon/index.jsx
+++ src/components/Icon/index.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import IcoMoon from "react-icomoon";
 const iconSet = require("./selection.json");
 
-const Icon = ({ ...props }) => <IcoMoon iconSet={iconSet} {...props} />;
+const Icon = ({ ...props }) => (
+  // [TODO]: Add reset fill feature to SVGPS!
+  // Then remove "disableFill" prop.
+  <IcoMoon iconSet={iconSet} disableFill {...props} />
+);
 
 export default Icon;
