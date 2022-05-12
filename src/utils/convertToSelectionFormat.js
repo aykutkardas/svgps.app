ff1f3227e303fd82adda998f50f053b014bd418a
Aykut Kardaş <aykutkrds@gmail.com>
2022-05-12 18:44:28 +0300

    Remove fill when single color

--- src/utils/convertToSelectionFormat.js
+++ src/utils/convertToSelectionFormat.js
@@ -1,4 +1,5 @@
 import svgpath from "svgpath";
+import _ from "lodash";
 
 function convertToSelectionFormat(icons) {
   const icomoonTemplate = {
@@ -10,10 +11,12 @@ function convertToSelectionFormat(icons) {
   icons.forEach((icon) => {
     const scale = 1024 / icon.width;
 
+    let fills = _.uniq(icon.fills).length === 1 ? [] : icon.fills;
+
     icomoonTemplate.icons.push({
       icon: {
         paths: icon.paths.map((path) => svgpath(path).scale(scale).toString()),
-        attrs: icon.fills.map((fill) => ({ fill })),
+        attrs: fills.map((fill) => ({ fill })),
         width: Math.round(
           (icon.height > icon.width ? icon.height : icon.width) * scale
         ),
