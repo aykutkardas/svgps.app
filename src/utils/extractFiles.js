b7a9fa8abafea198249ed08ab1cd4ef7379897f2
Aykut Kardaş <aykutkrds@gmail.com>
2022-05-14 12:51:05 +0300

    Refactor util (extractFiles)

--- src/utils/extractFiles.js
+++ src/utils/extractFiles.js
@@ -5,39 +5,40 @@ import { nanoid } from "nanoid";
 const extractFiles = async (event) => {
   const selectedIcons = [];
 
-  if (window.FileList && window.File && window.FileReader) {
-    for (let i = 0; i < event.target.files.length; i++) {
-      const file = event.target.files[i];
-      const blob = new Blob([file], { type: "text/svg" });
-
-      if (file && !file.type) {
-        toast.error(
-          "Error: The File.type property does not appear to be supported on this browser."
-        );
-        continue;
-      }
-
-      if (file.type !== "image/svg+xml") {
-        toast.error("Error: The selected file does not appear to be an svg.");
-        continue;
-      }
-
-      const content = await blob.text();
-
-      selectedIcons.push({
-        id: nanoid(),
-        name: getFormattedName(file.name),
-        content,
-        ...parse(content),
-      });
-    }
-
-    if (selectedIcons.length) {
-      toast.success("Upload successful!");
-    }
-
+  if (!window.FileList && !window.File && !window.FileReader) {
+    toast.error("Your browser does not support this feature");
     return selectedIcons;
   }
+
+  for (const file of event.target.files) {
+    if (file && !file.type) {
+      toast.error(
+        "Error: The File.type property does not appear to be supported on this browser."
+      );
+      continue;
+    }
+
+    if (file.type !== "image/svg+xml") {
+      toast.error("Error: The selected file does not appear to be an svg.");
+      continue;
+    }
+
+    const blob = new Blob([file], { type: "text/svg" });
+    const content = await blob.text();
+
+    selectedIcons.push({
+      id: nanoid(),
+      name: getFormattedName(file.name),
+      content,
+      ...parse(content),
+    });
+  }
+
+  if (selectedIcons.length) {
+    toast.success("Upload successful!");
+  }
+
+  return selectedIcons;
 };
 
 export default extractFiles;
