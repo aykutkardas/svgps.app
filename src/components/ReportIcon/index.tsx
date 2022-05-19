712fe961b11e74c7ddce4e93b4544cdeb0b3fbc7
Gizem <gizemnurkorkmaz@gmail.com>
2022-05-19 22:58:00 +0300

    Remove extra div and title element

--- src/components/ReportIcon/index.tsx
+++ src/components/ReportIcon/index.tsx
@@ -10,39 +10,32 @@ interface ReportIconProps {
 }
 
 const ReportIcon = ({ icon }: ReportIconProps) => {
-  const title = encodeURI("[Parse]: Type the title here...");
   const body = encodeURI(`
-  **User Agent:** 
-  \`${window.navigator.userAgent.replace(/;/g, ",")}\`
-
-  **Icon Detail:** 
-  \`\`\`js
-  \`${JSON.stringify(icon, null, 2)}\`
+  **SVG:** 
+  \`\`\`html
+  ${icon.content}
   \`\`\`
   ---
-  What is the problem you are experiencing?
   `);
 
   return (
     <>
       <ReactTooltip />
-      <div className={styles.ReportIcon}>
-        <a
-          className={styles.ReportIconLink}
-          href={`https://github.com/aykutkardas/svgps.app/issues/new?title=${title}&body=${body}`}
-          target="_blank"
-          rel="noreferrer"
-        >
-          <Icon
-            icon="bug"
-            size={10}
-            data-tip="Report a Problem"
-            data-background-color="var(--neutral-600)"
-            data-effect="solid"
-            onClick={(e) => e.stopPropagation()}
-          />
-        </a>
-      </div>
+      <a
+        className={styles.ReportIcon}
+        href={`https://github.com/aykutkardas/svgps.app/issues/new?&body=${body}`}
+        target="_blank"
+        rel="noreferrer"
+        onClick={(e) => e.stopPropagation()}
+      >
+        <Icon
+          icon="bug"
+          size={10}
+          data-tip="Report a Problem"
+          data-background-color="var(--neutral-600)"
+          data-effect="solid"
+        />
+      </a>
     </>
   );
 };
