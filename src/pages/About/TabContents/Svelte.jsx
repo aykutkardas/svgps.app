0aa7e94c2c88ac552cba53acf2c783bf955134f1
Gizem <gizemnurkorkmaz@gmail.com>
2022-06-02 20:26:14 +0300

    Add svelte content to About

---
+++ src/pages/About/TabContents/Svelte.jsx
@@ -0,0 +1,19 @@
+import styles from "../About.module.css";
+
+const TabContentSvelte = () => (
+  <div className={styles.Soon}>
+    <p>Details coming soon.</p>
+    <p>
+      <a
+        className={styles.Link}
+        href="https://github.com/aykutkardas/svelte-icomoon"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        svelte-icomoon
+      </a>
+    </p>
+  </div>
+);
+
+export default TabContentSvelte;
