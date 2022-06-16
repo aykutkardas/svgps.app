aed3a8f6027b27c2cb2265e526c64327e5699702
Gizem <gizemnurkorkmaz@gmail.com>
2022-06-16 14:21:23 +0300

    Remove twitter link in Footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,41 +1,10 @@
 import styles from "./Footer.module.css";
 
-import Icon from "src/components/Icon";
-
 import packageJson from "../../../package.json";
-
-const socials = [
-  {
-    name: "GitHub",
-    icon: "github",
-    iconSize: 22,
-    url: "https://github.com/aykutkardas/svgps.app",
-  },
-  {
-    name: "Twitter",
-    icon: "twitter",
-    iconSize: 20,
-    url: "https://twitter.com/aykutkardas",
-  },
-];
 
 const Footer = () => (
   <div className={styles.Footer}>
     <span>v{packageJson.version}</span>
-    <div className={styles.Social}>
-      {socials.map(({ name, icon, iconSize, url }) => (
-        <a
-          key={name}
-          href={url}
-          target="_blank"
-          rel="noreferrer"
-          aria-label={name}
-          className={styles.SocialLink}
-        >
-          <Icon icon={icon} size={iconSize} />
-        </a>
-      ))}
-    </div>
   </div>
 );
 
