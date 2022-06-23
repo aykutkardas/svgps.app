ed7e22276bdd82608586709d5b30adb4fb2fadbf
Aykut Kardaş <aykutkrds@gmail.com>
2022-06-23 23:34:51 +0300

    Fix types (Highlighter) (#59)

--- src/components/Highlighter/index.tsx
+++ src/components/Highlighter/index.tsx
@@ -1,8 +1,11 @@
-import SyntaxHighlighter from "react-syntax-highlighter";
-import { a11yDark } from "react-syntax-highlighter/dist/esm/styles/prism";
+import SyntaxHighlighter, {
+  SyntaxHighlighterProps,
+} from "react-syntax-highlighter";
+import { atomDark } from "react-syntax-highlighter/dist/esm/styles/prism";
 
-const Highlighter = ({ children, ...props }) => (
-  <SyntaxHighlighter style={a11yDark} {...props}>
+const Highlighter = ({ children, ...props }: SyntaxHighlighterProps) => (
+  // @ts-ignore [TODO]: Fix this
+  <SyntaxHighlighter style={atomDark} {...props}>
     {children}
   </SyntaxHighlighter>
 );
