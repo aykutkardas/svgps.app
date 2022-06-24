c0afedbfc98c17a0db60557497e34b335889654f
Gizem <gizemnurkorkmaz@gmail.com>
2022-06-25 00:46:33 +0300

    Switch to createRoot for root rendering in index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
