97b2ee10c0a883dbfd1d46531b6b9a307c8a489f
Aykut Kardaş <aykutkrds@gmail.com>
2022-06-24 11:03:51 +0300

    Fix stroke attributes (#60)

--- src/utils/convertToSelectionFormat.ts
+++ src/utils/convertToSelectionFormat.ts
@@ -1,6 +1,26 @@
 import svgpath from "svgpath";
 import uniq from "lodash.uniq";
 
+function clearEmptyAttr(attr) {
+  Object.keys(attr).forEach((key) => {
+    if (attr[key] === null) {
+      delete attr[key];
+    }
+  });
+
+  return attr;
+}
+
+function scaleStrokeWidth(attr, scale) {
+  const newAttr = { ...attr };
+
+  if (typeof newAttr.strokeWidth === "number") {
+    newAttr.strokeWidth = newAttr.strokeWidth * scale;
+  }
+
+  return newAttr;
+}
+
 function convertToSelectionFormat(icons) {
   const icomoonTemplate = {
     generatorSource: "svgps.app",
@@ -12,7 +32,26 @@ function convertToSelectionFormat(icons) {
     const scale = 1024 / icon.width;
 
     const fills = uniq(icon.fills).length === 1 ? [] : icon.fills;
-    const attrs = icon.attrs || [];
+    const singleStrokeColor =
+      uniq(icon.attrs.map(({ stroke }) => stroke)).length === 1;
+
+    const svgAttrs = clearEmptyAttr(icon.svgAttrs);
+
+    Object.keys(svgAttrs).forEach((key) => {
+      icon.attrs = icon.attrs.map((attr) => ({
+        ...attr,
+        [key]: svgAttrs[key],
+      }));
+    });
+
+    const attrs =
+      icon.attrs.map(clearEmptyAttr).map((attr) => {
+        const newAttr = scaleStrokeWidth(attr, scale);
+
+        if (singleStrokeColor) delete newAttr.stroke;
+
+        return newAttr;
+      }) || [];
 
     fills.forEach((fill, index) => {
       if (attrs[index]) {
