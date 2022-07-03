49cf1196185e713a945870ed76a43cd684fb5df7
Gizem <gizemnurkorkmaz@gmail.com>
2022-07-03 22:28:31 +0300

    Fix themeContext types (#73)

--- src/context/themeContext.tsx
+++ src/context/themeContext.tsx
@@ -1,17 +1,22 @@
 import { useState, createContext } from "react";
 import lookie from "lookie";
 
+type Theme = "dark" | "light";
+
 interface IThemeContext {
-  theme: string;
-  setTheme: (theme: string) => void;
+  theme: Theme;
+  setTheme: (theme: Theme) => void;
 }
 
 const initialTheme = lookie.get("theme") || "dark";
 
-const ThemeContext = createContext<IThemeContext>(initialTheme);
+const ThemeContext = createContext<IThemeContext>({
+  theme: initialTheme,
+  setTheme: () => {},
+});
 
 const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState(initialTheme);
+  const [theme, setTheme] = useState<Theme>(initialTheme);
 
   return (
     <ThemeContext.Provider value={{ theme, setTheme }}>
