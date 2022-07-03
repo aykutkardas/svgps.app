8563b916d0e2dbd75533ed54717cb2e1f1c8b6dc
Gizem <gizemnurkorkmaz@gmail.com>
2022-07-03 23:18:06 +0300

    Add esc shortcut to close Dialog (#74)

--- src/components/DialogBox/index.tsx
+++ src/components/DialogBox/index.tsx
@@ -1,4 +1,6 @@
+import { useHotkeys } from "react-hotkeys-hook";
 import cx from "classnames";
+
 import styles from "./DialogBox.module.css";
 
 import Button, { ButtonVariants } from "../Button";
@@ -17,29 +19,32 @@ const DialogBox = ({
   isOpen,
   setIsOpen,
   onConfirm,
-}: DialogBoxProps) => (
-  <div
-    className={cx(styles.Overlay, {
-      [styles.DialogOpen]: isOpen,
-    })}
-  >
-    <div className={styles.DialogBox}>
-      <div className={styles.DialogContent}>
-        <div className={styles.DialogText}>{children}</div>
-        {!hideButtons && (
-          <div className={styles.DialogButtons}>
-            <Button
-              variant={ButtonVariants.Ghost}
-              onClick={() => setIsOpen(false)}
-            >
-              Cancel
-            </Button>
-            <Button onClick={onConfirm}>Yes</Button>
-          </div>
-        )}
+}: DialogBoxProps) => {
+  useHotkeys("esc", () => setIsOpen(false));
+
+  return (
+    <div
+      className={cx(styles.Overlay, {
+        [styles.DialogOpen]: isOpen,
+      })}
+    >
+      <div className={styles.DialogBox}>
+        <div className={styles.DialogContent}>
+          <div className={styles.DialogText}>{children}</div>
+          {!hideButtons && (
+            <div className={styles.DialogButtons}>
+              <Button
+                variant={ButtonVariants.Ghost}
+                onClick={() => setIsOpen(false)}
+              >
+                Cancel
+              </Button>
+              <Button onClick={onConfirm}>Yes</Button>
+            </div>
+          )}
+        </div>
       </div>
     </div>
-  </div>
-);
-
+  );
+};
 export default DialogBox;
