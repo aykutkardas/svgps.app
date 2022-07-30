d5810b01dc65aff9844cb2fe5919a592388b97f8
Aykut Kardaş <aykutkrds@gmail.com>
2022-07-30 10:42:51 +0300

    Update tab (Svelte)

--- src/pages/About/TabContents/Svelte.tsx
+++ src/pages/About/TabContents/Svelte.tsx
@@ -1,4 +1,6 @@
+import Highlighter from "src/components/Highlighter";
 import Icon from "src/components/Icon";
+
 import styles from "../About.module.css";
 
 const TabContentSvelte = () => (
@@ -13,9 +15,52 @@ const TabContentSvelte = () => (
         <Icon icon="github" size={18} />
         svelte-icomoon
       </a>
+      <a
+        className={styles.Link}
+        href="https://www.npmjs.com/package/svelte-icomoon"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Icon icon="npm" size={16} color="#ea2039" />
+        npm
+      </a>
     </p>
+    <p>Install</p>
+    <Highlighter language="bash">{`npm install svelte-icomoon`}</Highlighter>
+    <Highlighter language="bash">{`yarn add svelte-icomoon`}</Highlighter>
+    <p>Define `Icon.svelte` component</p>
+    <Highlighter language="html">
+      {`
+<script>
+  import Icomoon from "svelte-icomoon";
 
-    <p>Details coming soon.</p>
+  import iconSet from "./selection.json";
+
+  export let name;
+  export let title;
+  export let color;
+  export let size = 16;
+  export let disableFill = false;
+  export let removeInitialStyle = false;
+  export const style = {};
+  const props = { 
+    name, title, color, size, disableFill, removeInitialStyle 
+  };
+</script>
+
+<Icomoon {...props} iconSet="{iconSet}" />
+        `}
+    </Highlighter>
+    <p>And use</p>
+    <Highlighter language="html">
+      {`
+<script>
+  import Icon from "./Icon.svelte";
+</script>
+
+<Icon name="pencil" size="{30}" color="blue" />
+        `}
+    </Highlighter>
   </div>
 );
 
