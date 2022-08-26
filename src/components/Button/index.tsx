45740ec9e00e66b9976a7b4781a57625de69dad9
Aykut Kardaş <aykutkrds@gmail.com>
2022-08-26 19:48:11 +0300

    Update button variant order.

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,20 +3,20 @@ import cx from "classnames";
 
 export enum ButtonVariants {
   Primary = "primary",
-  Ghost = "ghost",
   Secondary = "secondary",
   Export = "export",
+  Ghost = "ghost",
 }
 
 const variants = {
   [ButtonVariants.Primary]:
     "text-white bg-gradient-to-br hover:bg-gradient-to-bl from-violet-500 to-fuchsia-500",
-  [ButtonVariants.Ghost]:
-    "text-neutral-700 dark:text-neutral-400 hover:text-neutral-900 dark:hover:text-gray-300",
   [ButtonVariants.Secondary]:
     "text-white  bg-gradient-to-br hover:bg-gradient-to-bl from-green-500 to-lime-500",
   [ButtonVariants.Export]:
     "text-white  bg-gradient-to-br hover:bg-gradient-to-bl from-blue-500 to-sky-700",
+  [ButtonVariants.Ghost]:
+    "text-neutral-700 dark:text-neutral-400 hover:text-neutral-900 dark:hover:text-gray-300",
 };
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
