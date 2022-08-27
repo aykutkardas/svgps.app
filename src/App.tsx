adda6fe97de04be2eec141cf04233d866231c73f
Aykut Kardaş <aykutkrds@gmail.com>
2022-08-27 21:29:31 +0300

    Fix lint issues

--- src/App.tsx
+++ src/App.tsx
@@ -2,32 +2,33 @@ import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 
 import Header from "src/components/Header";
-import HomePage from "src/pages/HomePage";
-import AppPage from "src/pages/AppPage";
 import { ThemeProvider } from "src/context/themeContext";
 import { IconsProvider } from "src/context/iconsContext";
 
-export default function App() {
-  return (
-    <ThemeProvider>
-      <IconsProvider>
-        <Toaster
-          position="bottom-right"
-          toastOptions={{
-            className:
-              "bg-neutral-200 dark:bg-neutral-800 text-neutral-900 dark:text-neutral-50",
-          }}
-        />
-        <Router>
-          <div className="container mx-auto flex min-h-screen flex-col p-3">
-            <Header />
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/app" element={<AppPage />} />
-            </Routes>
-          </div>
-        </Router>
-      </IconsProvider>
-    </ThemeProvider>
-  );
-}
+import HomePage from "src/pages/HomePage";
+import AppPage from "src/pages/AppPage";
+
+const App = () => (
+  <ThemeProvider>
+    <IconsProvider>
+      <Toaster
+        position="bottom-right"
+        toastOptions={{
+          className:
+            "bg-neutral-200 dark:bg-neutral-800 text-neutral-900 dark:text-neutral-50",
+        }}
+      />
+      <Router>
+        <div className="container mx-auto flex min-h-screen flex-col p-3">
+          <Header />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/app" element={<AppPage />} />
+          </Routes>
+        </div>
+      </Router>
+    </IconsProvider>
+  </ThemeProvider>
+);
+
+export default App;
