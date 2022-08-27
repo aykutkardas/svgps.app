e3c884843d48544a3ebfac8e3f978d538ded0eaa
Aykut Kardaş <aykutkrds@gmail.com>
2022-08-27 15:49:34 +0300

    Remove flex value

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -8,7 +8,7 @@ const About = () => {
 
   return (
     <div className="my-auto flex h-full max-w-full flex-col lg:flex-row">
-      <div className="flex h-screen w-full flex-1 items-center md:h-auto">
+      <div className="flex h-screen w-full items-center md:h-auto">
         <div className="flex w-full max-w-[600px] flex-col items-start justify-center">
           <h2 className="mb-3 bg-gradient-to-r from-purple-500 to-pink-700 bg-clip-text text-5xl font-bold text-transparent">
             No need for many files.
@@ -36,7 +36,7 @@ const About = () => {
           </Button>
         </div>
       </div>
-      <div className="mx-0 mt-6  hidden h-screen flex-1 sm:mx-2 sm:mt-0 md:h-auto lg:flex">
+      <div className="mx-0 mt-6  hidden h-screen sm:mx-2 sm:mt-0 md:h-auto lg:flex">
         <Example />
       </div>
     </div>
