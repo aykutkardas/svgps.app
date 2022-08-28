33dd095c90f9715abfcb0a109d63aebe275f0e33
Aykut Kardaş <aykutkrds@gmail.com>
2022-08-28 19:40:29 +0300

    Refactor (ThemeSwitcher) (#99)

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -1,9 +1,8 @@
 import { useEffect, useContext } from "react";
 import lookie from "lookie";
-import cx from "classnames";
-import { Switch } from "@headlessui/react";
 
 import { ThemeContext } from "src/context/themeContext";
+import Icon from "src/components/Icon";
 
 const ThemeSwitcher = () => {
   const { theme, setTheme } = useContext(ThemeContext);
@@ -13,33 +12,22 @@ const ThemeSwitcher = () => {
     lookie.set("theme", theme);
   }, [theme]);
 
+  const isDark = theme === "dark";
+
   const handleTheme = () => {
-    const newTheme = theme === "light" ? "dark" : "light";
+    const newTheme = isDark ? "light" : "dark";
     setTheme(newTheme);
   };
 
-  const isChecked = theme === "dark";
+  const icon = isDark ? "moon" : "sun";
 
   return (
-    <Switch
-      checked={isChecked}
-      onChange={handleTheme}
-      className={cx(
-        "relative inline-flex h-[20px] w-[35px] shrink-0 cursor-pointer rounded-full border-2 border-transparent",
-        "transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75",
-        isChecked ? "bg-neutral-200" : "bg-neutral-700"
-      )}
-    >
-      <span className="sr-only">Use theme switch</span>
-      <span
-        aria-hidden="true"
-        className={cx(
-          "pointer-events-none inline-block h-[16px] w-[16px] transform rounded-full ring-0 transition duration-200 ease-in-out",
-          "bg-white shadow-lg dark:bg-neutral-900",
-          isChecked ? "translate-x-[15px]" : "translate-x-0"
-        )}
-      />
-    </Switch>
+    <Icon
+      icon={icon}
+      size={20}
+      onClick={handleTheme}
+      className="cursor-pointer select-none text-neutral-700 hover:text-neutral-900 dark:text-neutral-100 dark:hover:text-neutral-300"
+    />
   );
 };
 
