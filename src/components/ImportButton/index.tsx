5f24e553ed78dc032ff60b026bd5856cd3ea12b0
Aykut Kardaş <aykutkrds@gmail.com>
2022-08-28 12:19:54 +0300

    Fix popover close issue (ImportButton)

--- src/components/ImportButton/index.tsx
+++ src/components/ImportButton/index.tsx
@@ -25,22 +25,33 @@ const ImportButton = ({ className }) => (
         leaveFrom="opacity-100 translate-y-0"
         leaveTo="opacity-0 translate-y-1"
       >
-        <Popover.Panel className="absolute left-1/2 z-10 mt-3 w-72 max-w-lg -translate-x-1/2 transform px-4">
-          <div className="flex flex-col gap-2 rounded-lg bg-neutral-700 p-4">
-            <p className="text-center text-sm text-neutral-200">
-              Do you want to import SVG files to the set or import a JSON file?
-            </p>
-            <ImportWrapper>
-              <Button className="w-full bg-purple-500 text-white hover:bg-purple-400">
-                Import SVG Files
-              </Button>
-            </ImportWrapper>
-            <ImportWrapper type="JSON">
-              <Button className="w-full bg-sky-500 text-white hover:bg-sky-400">
-                Import JSON
-              </Button>
-            </ImportWrapper>
-          </div>
+        <Popover.Panel className="absolute right-0 z-10 mt-2 w-72 max-w-lg  transform">
+          {({ close }) => (
+            <div className="flex flex-col gap-2 rounded-lg  border border-neutral-200 bg-neutral-100 p-4 shadow-xl dark:border-neutral-700 dark:bg-neutral-800">
+              <p className="text-center text-sm text-neutral-600 dark:text-neutral-200">
+                Do you want to import SVG files to the set or import a JSON
+                file?
+              </p>
+              <div className="mt-3 flex justify-center gap-2">
+                <ImportWrapper>
+                  <Button
+                    className="w-full bg-fuchsia-500 text-white hover:bg-fuchsia-400"
+                    onClick={() => close()}
+                  >
+                    Import SVG
+                  </Button>
+                </ImportWrapper>
+                <ImportWrapper type="JSON">
+                  <Button
+                    className="w-full bg-sky-500 text-white hover:bg-sky-400"
+                    onClick={() => close()}
+                  >
+                    Import JSON
+                  </Button>
+                </ImportWrapper>
+              </div>
+            </div>
+          )}
         </Popover.Panel>
       </Transition>
     </Popover>
