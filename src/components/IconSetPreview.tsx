f149ff8c1d80f2f41103b20f100978dbba77606d
Aykut Kardaş <aykutkrds@gmail.com>
2022-08-30 21:40:57 +0300

    Fix responsive (IconSetPreview)

--- src/components/IconSetPreview.tsx
+++ src/components/IconSetPreview.tsx
@@ -70,7 +70,7 @@ const IconSetPreview = () => {
         title="Remove All"
         description="Are you sure you want to clear all icons?"
       />
-      <div className="flex h-20 flex-col items-center justify-between gap-3 divide-neutral-300 p-4 dark:divide-neutral-800 sm:flex-row">
+      <div className="min-h-20 flex flex-col items-center justify-between gap-3 divide-neutral-300 p-4 dark:divide-neutral-800 sm:flex-row">
         <div className="text-xs text-neutral-500">{`${icons.length} icons`}</div>
         {!noIcons && (
           <div className="order-1 flex flex-col gap-3 sm:order-2 sm:flex-row">
