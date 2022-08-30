57abd8a2b5c45c83b6ac82592c7ba8c26d83ebb4
Aykut Kardaş <aykutkrds@gmail.com>
2022-08-30 19:43:42 +0300

    Add sponsor link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,14 @@
 import packageJson from "../../package.json";
 
 const Footer = () => (
-  <footer className="text-center text-xs font-medium text-neutral-500">
-    v{packageJson.version}
+  <footer className="flex flex-col items-center justify-center text-xs font-medium text-neutral-500">
+    <a
+      href="https://github.com/aykutkardas/svgps.app#become-a-sponsor-to-core-maintainers"
+      className="text-purple-500"
+    >
+      Become a Sponsor
+    </a>
+    <span className="mt-1 font-roboto">v{packageJson.version}</span>
   </footer>
 );
 
