d590b6ebb3419bc15003aaa2fc8a6a76d83ffad8
Aykut Kardaş <aykutkrds@gmail.com>
2022-08-30 18:57:58 +0300

    Fix delete issue when searching (IconSetPreview) (#113)
    * Fix different style
    
    * Fix delete issue when searching (IconSetPreview)

--- src/components/IconSetPreview.tsx
+++ src/components/IconSetPreview.tsx
@@ -9,38 +9,26 @@ import NewIconBox from "src/components/NewIconBox";
 import ExportButton from "src/components/ExportButton";
 import ImportButton from "src/components/ImportButton";
 import { IconsContext } from "src/context/IconsContext";
-import { IconSetItem } from "src/types";
 
 const IconSetPreview = () => {
   const { icons, setIcons } = useContext(IconsContext);
   const [isDialogOpen, setIsDialogOpen] = useState(false);
   const [search, setSearch] = useState("");
-  const [filteredIcons, setFilteredIcons] = useState<IconSetItem[]>([]);
   const selectedIcons = icons.filter((icon) => icon.__meta?._selected);
   const selectionCount = selectedIcons.length;
 
-  const handleSearch = ({ target }) => {
-    const searchKey = target.value;
-    let newIcons = [];
-
-    if (searchKey) {
-      newIcons = icons.filter((icon) =>
-        icon.properties?.name.toLowerCase().includes(searchKey.toLowerCase())
-      );
-    }
-
-    setSearch(searchKey);
-    setFilteredIcons(newIcons);
-  };
+  const handleSearch = ({ target }) => setSearch(target.value);
 
   const clearAll = () => {
     setIcons([]);
     setIsDialogOpen(false);
   };
 
-  const iconList = search ? filteredIcons : icons;
+  let filteredIcons = icons.filter((icon) =>
+    icon.properties?.name.toLowerCase().includes(search.toLowerCase())
+  );
 
-  const noIcons = iconList.length === 0;
+  const noIcons = filteredIcons.length === 0;
 
   return (
     <div
@@ -52,13 +40,13 @@ const IconSetPreview = () => {
       <div className="flex items-center justify-between p-4">
         <label
           className={cx(
-            "inline-flex w-64 items-center bg-neutral-200 bg-transparent py-1 text-neutral-300",
+            "inline-flex w-64 items-center bg-neutral-200 bg-transparent py-1 text-neutral-400",
             { "cursor-not-allowed opacity-40": noIcons && !search }
           )}
         >
-          <Icon icon="search" size={16} className="text-neutral-400" />
+          <Icon icon="search" size={16} className="text-current" />
           <input
-            className="ml-2 h-6 w-16 rounded-sm border-none bg-transparent text-sm outline-none disabled:cursor-not-allowed"
+            className="ml-2 h-6 w-full rounded-sm border-none bg-transparent text-sm text-current outline-none disabled:cursor-not-allowed"
             onKeyUp={handleSearch}
             placeholder="Search..."
             disabled={noIcons && !search}
@@ -70,7 +58,7 @@ const IconSetPreview = () => {
         {search && noIcons && (
           <p className="w-48 p-4 text-sm text-neutral-500">No icons found.</p>
         )}
-        {iconList.map((icon) => (
+        {filteredIcons.map((icon) => (
           <IconBox key={icon.__meta?.id} icon={icon} />
         ))}
         {!search && <NewIconBox />}
@@ -83,7 +71,7 @@ const IconSetPreview = () => {
         description="Are you sure you want to clear all icons?"
       />
       <div className="flex h-20 flex-col items-center justify-between gap-3 divide-neutral-300 p-4 dark:divide-neutral-800 sm:flex-row">
-        <div className="text-xs text-neutral-500">{`${iconList.length} icons`}</div>
+        <div className="text-xs text-neutral-500">{`${icons.length} icons`}</div>
         {!noIcons && (
           <div className="order-1 flex flex-col gap-3 sm:order-2 sm:flex-row">
             <Button
