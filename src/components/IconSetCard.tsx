c5fe61054b4f24c7f96f2d388c929f478fd27d56
Gizem <gizemnurkorkmaz@gmail.com>
2022-09-11 11:52:11 +0300

    Fix html tags

--- src/components/IconSetCard.tsx
+++ src/components/IconSetCard.tsx
@@ -13,16 +13,16 @@ const IconSetCard = ({ iconSet }: IconSetCardProps) => (
   <div className="m-5 flex w-80 cursor-pointer flex-col gap-6 border border-neutral-700 p-5 hover:border-neutral-500">
     <div className="align-center flex justify-between">
       <div>
-        <p className="text-lg text-neutral-600 dark:text-neutral-300">
+        <h2 className="text-lg text-neutral-600 dark:text-neutral-300">
           {iconSet.name}
-        </p>
-        <p className="pt-1 text-sm text-neutral-500 dark:text-neutral-400">
+        </h2>
+        <h3 className="pt-1 text-sm text-neutral-500 dark:text-neutral-400">
           {iconSet.creator}
-        </p>
+        </h3>
       </div>
-      <p className="text-sm text-neutral-600 dark:text-neutral-500">
+      <div className="text-sm text-neutral-600 dark:text-neutral-500">
         {iconSet.licence}
-      </p>
+      </div>
     </div>
 
     <div className="mt-5 flex justify-between">
@@ -31,9 +31,9 @@ const IconSetCard = ({ iconSet }: IconSetCardProps) => (
           <Icon icon={name} className="mr-1 h-5 w-5 text-neutral-500" />
         ))}
       </div>
-      <p className="pt-2 text-xs text-neutral-400">
+      <div className="pt-2 text-xs text-neutral-400">
         {iconSet.icons.length} icons
-      </p>
+      </div>
     </div>
   </div>
 );
