6a45a77563094ee59469e4ab0f35795b81b8d773
Aykut Kardaş <aykutkrds@gmail.com>
2022-09-15 00:03:16 +0300

    Update (SupportButton)

---
+++ src/components/SupportButton.tsx
@@ -0,0 +1,20 @@
+import cx from "clsx";
+
+import Icon from "src/components/Icon";
+
+const SupportButton = () => (
+  <a
+    href="https://www.buymeacoffee.com/aykutkardas"
+    target="_blank"
+    rel="noreferrer"
+    aria-label="Buy Me a Coffee"
+    title="Buy Me a Coffee"
+    className={cx(
+      "absolute bottom-4 right-4 inline-flex h-10 w-10 cursor-pointer items-center justify-center rounded-full bg-gradient-to-tr from-yellow-600 to-yellow-500 text-white shadow-xl transition hover:scale-110"
+    )}
+  >
+    <Icon icon="coffee" size={22} />
+  </a>
+);
+
+export default SupportButton;
