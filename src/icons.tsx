792b284eebfae6eff7ddec37dc9069583d0f82de
Aykut Kardaş <aykutkrds@gmail.com>
2022-09-18 10:45:50 +0300

    Add new icon sets (/store) (#134)
    * Add feather icon set (/store)
    
    * Add google material icon set (/store)

--- src/icons.tsx
+++ src/icons.tsx
@@ -2,6 +2,8 @@ import SimpleLineIcons from "./assets/icons/demo/simple-line-icons.json";
 import ElementPlus from "./assets/icons/demo/element-plus.json";
 import Nimbus from "./assets/icons/demo/nimbus.json";
 import Zondicons from "./assets/icons/demo/zondicons.json";
+import Feather from "./assets/icons/demo/feather.json";
+import GoogleMaterialIcons from "./assets/icons/demo/google-material-icons.json";
 
 const icons = [
   {
@@ -35,6 +37,22 @@ const icons = [
     licence: "MIT",
     count: 301,
     icons: Zondicons,
+  },
+  {
+    slug: "feather",
+    name: "Feather",
+    creator: "Feather",
+    licence: "MIT",
+    count: 287,
+    icons: Feather,
+  },
+  {
+    slug: "google-material-icons",
+    name: "Google Material Icons",
+    creator: "Material Design Authors",
+    licence: "Apache 2.0",
+    count: 2191,
+    icons: GoogleMaterialIcons,
   },
 ];
 
