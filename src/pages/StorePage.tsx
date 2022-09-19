106a3d927e69615d08c1d2e106f433303a782872
Aykut Kardaş <aykutkrds@gmail.com>
2022-09-19 09:05:32 +0300

    Fix unique keys

--- src/pages/StorePage.tsx
+++ src/pages/StorePage.tsx
@@ -7,6 +7,7 @@ const IconsPage = () => (
   <div className="my-auto flex w-full flex-wrap justify-center">
     {icons.map((iconSet) => (
       <Link
+        key={iconSet.slug}
         className="w-full p-3 sm:w-1/2 md:w-1/3 lg:w-1/4"
         to={`/store/${iconSet.slug}`}
       >
