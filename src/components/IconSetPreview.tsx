59bc44f591a52136e0e0da3549589f1d98151ea8
Aykut Kardaş <aykutkrds@gmail.com>
2022-09-21 08:03:43 +0300

    Add send to app selected icons support (IconSetPreview)

--- src/components/IconSetPreview.tsx
+++ src/components/IconSetPreview.tsx
@@ -40,6 +40,18 @@ const IconSetPreview = ({ iconSet, data }) => {
     toast.success("Icon sent to App!");
   };
 
+  const handleSendToAppSelected = () => {
+    const newAppIcons = [
+      ...appIcons,
+      ...selectedIcons.map((icon) => {
+        delete icon.__meta?._selected;
+        return icon;
+      }),
+    ];
+    setAppIcons(newAppIcons);
+    toast.success("Icons sent to App!");
+  };
+
   const noIcons = filteredIcons.length === 0;
 
   return (
@@ -166,6 +178,17 @@ const IconSetPreview = ({ iconSet, data }) => {
       <div className="z-10 flex h-20 flex-col items-center justify-between gap-3 divide-neutral-300 bg-neutral-100 p-4 dark:divide-neutral-800 dark:bg-neutral-800 sm:flex-row">
         <div className="text-xs text-neutral-500">{`${icons.length} icons`}</div>
         <div className="order-1 flex flex-col gap-3 sm:order-2 sm:flex-row">
+          {selectionCount > 0 && (
+            <Button
+              className="order-2 bg-purple-500 text-white"
+              onClick={handleSendToAppSelected}
+            >
+              Send to App
+              <span className="ml-1 inline-flex h-5 w-5 items-center justify-center rounded-full bg-purple-700 text-xs">
+                {selectionCount}
+              </span>
+            </Button>
+          )}
           {selectionCount > 0 && (
             <ExportButton
               variant={ButtonVariants.Secondary}
