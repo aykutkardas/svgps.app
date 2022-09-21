656dd49aa2a283d9bd61d5e3b5f425bf1518b434
Aykut Kardaş <aykutkrds@gmail.com>
2022-09-21 08:17:52 +0300

    Improve UI (IconSetPreview) (#150)

--- src/components/IconSetPreview.tsx
+++ src/components/IconSetPreview.tsx
@@ -36,7 +36,7 @@ const IconSetPreview = ({ iconSet, data }) => {
   return (
     <div
       className={cx(
-        "flex h-[600px] flex-col divide-y rounded-lg border shadow-xl dark:divide-neutral-700 dark:border-neutral-700 dark:bg-neutral-800",
+        "relative flex h-[600px] flex-col divide-y overflow-hidden rounded-lg border shadow-xl dark:divide-neutral-700 dark:border-neutral-700 dark:bg-neutral-800",
         "divide-neutral-200 border-neutral-200 bg-neutral-100"
       )}
     >
@@ -81,8 +81,10 @@ const IconSetPreview = ({ iconSet, data }) => {
       </div>
       <div
         className={cx(
-          "relative grid snap-y grid-cols-4 gap-1 overflow-y-auto overflow-x-hidden py-8 px-0 transition sm:grid-cols-7 md:grid-cols-8 lg:grid-cols-10 xl:grid-cols-14",
-          noIcons ? "h-52" : "max-h-[450px]"
+          "relative flex-1 snap-y grid-cols-4 gap-1 overflow-y-auto overflow-x-hidden py-8 px-0 pb-20 transition",
+          noIcons
+            ? "flex items-center justify-center"
+            : "grid sm:grid-cols-7 md:grid-cols-8 lg:grid-cols-10 xl:grid-cols-14"
         )}
       >
         {search && noIcons && !isDragging && (
@@ -101,42 +103,51 @@ const IconSetPreview = ({ iconSet, data }) => {
         ))}
       </div>
 
-      {inspectedIcon && (
-        <div className="min-h-20 flex flex-col items-center justify-between gap-3 divide-neutral-300 p-4 dark:divide-neutral-800 sm:flex-row">
-          <div className="flex items-center text-sm text-neutral-800 dark:text-neutral-50">
-            <Icon
-              iconSet={iconSet}
-              icon={inspectedIcon.properties.name}
-              size={40}
-              className="mr-2"
-            />
-            {inspectedIcon.properties.name}
-          </div>
-          <div className="order-1 flex flex-col gap-3 sm:order-2 sm:flex-row">
-            <Button
-              variant={ButtonVariants.Ghost}
-              className="px-1"
-              onClick={handleCopy}
-            >
-              Copy SVG
-            </Button>
-            <Button
-              className="px-1"
-              variant={ButtonVariants.Ghost}
-              onClick={() =>
-                downloadSVG(
-                  inspectedIcon.properties.name,
-                  convertToSVG(inspectedIcon)
-                )
-              }
-            >
-              Download SVG
-            </Button>
-          </div>
+      <div
+        className={cx(
+          "absolute flex h-20 w-full flex-col items-center justify-between gap-3 divide-neutral-300 bg-white/[0.01]  p-4 backdrop-blur-lg transition-all duration-300 dark:divide-neutral-800 sm:flex-row",
+          inspectedIcon ? "bottom-20" : "bottom-0"
+        )}
+      >
+        <Icon
+          icon="close"
+          size={16}
+          className="absolute top-0 right-0 m-1 cursor-pointer text-neutral-800 hover:text-neutral-900 dark:text-neutral-400 dark:hover:text-neutral-300"
+          onClick={() => setInspectedIcon(null)}
+        />
+        <div className="flex items-center text-sm text-neutral-700 dark:text-neutral-200">
+          <Icon
+            iconSet={iconSet}
+            icon={inspectedIcon?.properties.name}
+            size={33}
+            className="mr-2"
+          />
+          {inspectedIcon?.properties.name}
         </div>
-      )}
+        <div className="order-1 flex flex-col gap-3 sm:order-2 sm:flex-row">
+          <Button
+            variant={ButtonVariants.Ghost}
+            className="px-1"
+            onClick={handleCopy}
+          >
+            Copy SVG
+          </Button>
+          <Button
+            className="px-1"
+            variant={ButtonVariants.Ghost}
+            onClick={() =>
+              downloadSVG(
+                inspectedIcon?.properties.name,
+                convertToSVG(inspectedIcon)
+              )
+            }
+          >
+            Download SVG
+          </Button>
+        </div>
+      </div>
 
-      <div className="min-h-20 flex flex-col items-center justify-between gap-3 divide-neutral-300 p-4 dark:divide-neutral-800 sm:flex-row">
+      <div className="z-10 flex h-20 flex-col items-center justify-between gap-3 divide-neutral-300 bg-neutral-100 p-4 dark:divide-neutral-800 dark:bg-neutral-800 sm:flex-row">
         <div className="text-xs text-neutral-500">{`${icons.length} icons`}</div>
         <div className="order-1 flex flex-col gap-3 sm:order-2 sm:flex-row">
           {selectionCount > 0 && (
