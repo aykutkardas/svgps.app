211b8a429b3dee03da3d0c9df4689145166f1ca7
Aykut Kardaş <aykutkrds@gmail.com>
2022-09-23 19:51:39 +0300

    Add total icon count (store)

--- src/pages/StorePage.tsx
+++ src/pages/StorePage.tsx
@@ -4,27 +4,39 @@ import IconSetCard from "src/components/IconSetCard";
 
 import icons from "../icons";
 
-const IconsPage = () => (
-  <div className="my-auto flex w-full flex-wrap justify-center">
-    <Helmet>
-      <title>SVGPS - Icon Store</title>
-    </Helmet>
-    {icons.map((iconSet) => (
-      <Link
-        key={iconSet.slug}
-        className="w-full p-3 sm:w-1/2 md:w-1/3 lg:w-1/4"
-        to={`/store/${iconSet.slug}`}
-      >
-        <IconSetCard
-          name={iconSet.name}
-          creator={iconSet.creator}
-          licence={iconSet.licence}
-          count={iconSet.count}
-          iconSet={iconSet.icons}
-        />
-      </Link>
-    ))}
-  </div>
-);
+const IconsPage = () => {
+  const iconCount = icons.reduce((acc, iconSet) => acc + iconSet.count, 0);
+
+  return (
+    <div className="my-auto mt-[20%] flex w-full flex-col md:mt-[10%]">
+      <Helmet>
+        <title>SVGPS - Icon Store</title>
+      </Helmet>
+      <div className="mb-10 flex flex-col items-center justify-center dark:text-white">
+        <p className="text-sm text-neutral-600 dark:text-neutral-400">
+          Choose what you want from{" "}
+          <b className="mx-1 text-purple-500">{iconCount} icons</b> and use it.
+        </p>
+      </div>
+      <div className="flex flex-wrap justify-center">
+        {icons.map((iconSet) => (
+          <Link
+            key={iconSet.slug}
+            className="w-full p-3 sm:w-1/2 md:w-1/3 lg:w-1/4"
+            to={`/store/${iconSet.slug}`}
+          >
+            <IconSetCard
+              name={iconSet.name}
+              creator={iconSet.creator}
+              licence={iconSet.licence}
+              count={iconSet.count}
+              iconSet={iconSet.icons}
+            />
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default IconsPage;
