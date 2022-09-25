5b0a41d1b5dd8eadfa0e96574aa0bc94ea7ee693
Aykut Kardaş <aykutkrds@gmail.com>
2022-09-25 20:00:07 +0300

    Refactor header (IconSetPreviewHeader, IconsAppHeader)

---
+++ src/components/IconSetSearch.tsx
@@ -0,0 +1,26 @@
+import cx from "clsx";
+
+import Icon from "src/components/Icon";
+
+const IconSetSearch = ({ disabled = false, setSearch }) => {
+  const handleSearch = ({ target }) => setSearch(target.value);
+
+  return (
+    <label
+      className={cx(
+        "inline-flexitems-center bg-neutral-200 bg-transparent py-1 text-neutral-400",
+        { "cursor-not-allowed opacity-40": disabled }
+      )}
+    >
+      <Icon icon="search" size={16} className="text-current" />
+      <input
+        className="ml-2 h-6 w-16 rounded-sm border-none bg-transparent text-sm text-current outline-none transition-all focus:w-44 disabled:cursor-not-allowed"
+        onKeyUp={handleSearch}
+        placeholder="Search..."
+        disabled={disabled}
+      />
+    </label>
+  );
+};
+
+export default IconSetSearch;
--- src/components/IconsAppHeader.tsx
+++ src/components/IconsAppHeader.tsx
@@ -1,30 +1,11 @@
-import cx from "clsx";
-
-import Icon from "src/components/Icon";
 import ImportButton from "src/components/ImportButton";
+import IconSetSearch from "./IconSetSearch";
 
-const IconsAppHeader = ({ noIcons, search, setSearch }) => {
-  const handleSearch = ({ target }) => setSearch(target.value);
-
-  return (
-    <div className="flex items-center justify-between p-4">
-      <label
-        className={cx(
-          "inline-flex w-64 items-center bg-neutral-200 bg-transparent py-1 text-neutral-400",
-          { "cursor-not-allowed opacity-40": noIcons && !search }
-        )}
-      >
-        <Icon icon="search" size={16} className="text-current" />
-        <input
-          className="ml-2 h-6 w-full rounded-sm border-none bg-transparent text-sm text-current outline-none disabled:cursor-not-allowed"
-          onKeyUp={handleSearch}
-          placeholder="Search..."
-          disabled={noIcons && !search}
-        />
-      </label>
-      <ImportButton className="order-2 sm:order-1" />
-    </div>
-  );
-};
+const IconsAppHeader = ({ noIcons, search, setSearch }) => (
+  <div className="flex items-center justify-between p-4">
+    <ImportButton />
+    <IconSetSearch setSearch={setSearch} disabled={noIcons && !search} />
+  </div>
+);
 
 export default IconsAppHeader;
