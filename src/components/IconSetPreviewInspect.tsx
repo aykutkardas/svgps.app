6b594961faaa25891a58aa252f8ad825f407451c
Aykut Kardaş <aykutkrds@gmail.com>
2022-09-27 23:34:37 +0300

    Fix typo

--- src/components/IconSetPreviewInspect.tsx
+++ src/components/IconSetPreviewInspect.tsx
@@ -29,7 +29,7 @@ const IconSetPreviewInspect = ({
 
   const handleCopyJSX = () => {
     copy(convertToJSX(inspectedIcon, size));
-    toast.success("SVG Copied!");
+    toast.success("JSX Copied!");
   };
 
   const handleSendToApp = () => {
