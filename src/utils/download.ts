b67ac142a439ff31d57328986a5aaab25295ff1a
Aykut Kardaş <aykutkrds@gmail.com>
2022-09-28 00:12:04 +0300

    Add download support for multiple files (#186)

---
+++ src/utils/download.ts
@@ -0,0 +1,8 @@
+export const download = (blob: Blob, name: string) => {
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = name;
+  a.click();
+  a.remove();
+};
