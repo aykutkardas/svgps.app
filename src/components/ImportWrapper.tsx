af92e28830ccffd28aac3bae85703c1282cee1f9
Aykut Kardaş <aykutkrds@gmail.com>
2022-10-08 09:25:03 +0300

    Refactor file upload flow

--- src/components/ImportWrapper.tsx
+++ src/components/ImportWrapper.tsx
@@ -1,19 +1,15 @@
 import { useRef, useContext } from "react";
-import toast from "react-hot-toast";
 
-import extractFiles from "src/utils/extractFiles";
-import extractJSON from "src/utils/extractJSON";
+import { importFiles } from "src/utils/extractFiles";
 import { IconsContext } from "src/context/IconsContext";
 
 interface ImportWrapperProps {
-  type?: "SVG" | "JSON";
   children: React.ReactNode;
   onComplete?: () => void;
   className?: string;
 }
 
 const ImportWrapper = ({
-  type = "SVG",
   onComplete,
   className = "",
   children,
@@ -22,47 +18,16 @@ const ImportWrapper = ({
 
   const fileInput = useRef<null | HTMLInputElement>();
 
-  const handleSvgFilesUpload = async (event) => {
-    const importedIcons = await extractFiles(event);
-
-    if (!importedIcons.length) return;
-
-    const oldIcons = [...icons].map((icon) => {
-      const matchedIcon = importedIcons.find(
-        ({ properties }) => properties.name === icon.properties.name
-      );
-
-      return matchedIcon || icon;
+  const handleUpload = (event) =>
+    importFiles(event, icons, (newIcons) => {
+      setIcons(newIcons);
+      onComplete?.();
     });
-
-    const newIcons = importedIcons.filter(
-      ({ properties }) =>
-        !oldIcons.find((oldIcon) => oldIcon.properties.name === properties.name)
-    );
-
-    if (newIcons.length) {
-      toast.success("Import successful!");
-    }
-
-    setIcons([...oldIcons, ...newIcons]);
-    onComplete?.();
-  };
-
-  const handleJsonFileUpload = async (event) => {
-    const importedIcons = await extractJSON(event);
-
-    if (!importedIcons.length) return;
-
-    setIcons(importedIcons);
-    onComplete?.();
-  };
 
   const handleClick = (e) => {
     e.preventDefault();
     fileInput?.current?.click();
   };
-
-  const isJsonType = type === "JSON";
 
   // If a deleted icon is re-imported, the import will not work stable.
   // "onChange" event doesn't work at all. This key was required to fix this issue.
@@ -75,9 +40,9 @@ const ImportWrapper = ({
         className="hidden"
         ref={fileInput}
         type="file"
-        multiple={isJsonType ? false : true}
-        accept={isJsonType ? "application/json" : "image/svg+xml"}
-        onChange={isJsonType ? handleJsonFileUpload : handleSvgFilesUpload}
+        multiple={true}
+        accept="application/json,image/svg+xml"
+        onChange={handleUpload}
       />
       <span onClick={handleClick}>{children}</span>
     </label>
