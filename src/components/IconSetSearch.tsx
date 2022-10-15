464730ab78d4eefe8ca28fdd1509137374c4fc1d
Batın Eryılmaz <batin.eryilmaz@trendyol.com>
2022-10-15 16:54:54 +0200

    Add remove text icon when search bar is not empty

--- src/components/IconSetSearch.tsx
+++ src/components/IconSetSearch.tsx
@@ -2,23 +2,36 @@ import clsx from "clsx";
 
 import Icon from "src/components/Icon";
 
-const IconSetSearch = ({ disabled = false, setSearch }) => {
+const IconSetSearch = ({ disabled = false, search, setSearch }) => {
   const handleSearch = ({ target }) => setSearch(target.value);
+  const clearSearch = (e) => {
+    e.preventDefault();
+    setSearch("");
+  };
 
   return (
     <label
       className={clsx(
-        "inline-flex items-center bg-transparent py-1 text-neutral-400",
+        "inline-flex items-center bg-transparent py-1 text-neutral-400 position: relative",
         { "cursor-not-allowed opacity-40": disabled }
       )}
     >
       <Icon icon="search" size={16} className="text-current" />
       <input
+        value={search}
         className="ml-2 h-6 w-16 rounded-sm border-none bg-transparent text-sm text-current outline-none transition-all focus:w-44 disabled:cursor-not-allowed"
-        onKeyUp={handleSearch}
+        onChange={handleSearch}
         placeholder="Search..."
         disabled={disabled}
       />
+      {search.length > 0 && (
+        <Icon
+          icon="x-circle"
+          size={16}
+          className="right-0 ml-2 position: absolute cursor-pointer text-current"
+          onClick={clearSearch}
+        />
+      )}
     </label>
   );
 };
