c5dd2d08e4de0b5f7321ae8785cc25ee98160fc9
Aykut Kardaş <aykutkrds@gmail.com>
2022-12-03 19:26:27 +0300

    Update component

--- src/components/Sample/CodeHighlight.tsx
+++ src/components/Sample/CodeHighlight.tsx
@@ -4,28 +4,26 @@ import { atelierCaveDark } from "react-syntax-highlighter/dist/esm/styles/hljs";
 import Icon from "src/components/Icon";
 import Button from "../Button";
 
-const CodeHighlight = ({ data, onCopyCode }) => {
-  return (
-    <div className="group relative">
-      <Button
-        variant="icon"
-        onClick={onCopyCode}
-        className="sticky left-full top-2 mr-2 text-neutral-500 opacity-0 group-hover:opacity-50"
-      >
-        <Icon size={24} icon="copy" />
-      </Button>
-      <SyntaxHighlighter
-        language={data.syntax}
-        style={atelierCaveDark}
-        showLineNumbers
-        customStyle={{
-          marginTop: -42,
-        }}
-      >
-        {data.sample}
-      </SyntaxHighlighter>
-    </div>
-  );
-};
+const CodeHighlight = ({ data, onCopyCode }) => (
+  <div className="group relative">
+    <Button
+      variant="icon"
+      onClick={onCopyCode}
+      className="sticky left-full top-2 mr-2 text-neutral-500 opacity-0 group-hover:opacity-50"
+    >
+      <Icon size={24} icon="copy" />
+    </Button>
+    <SyntaxHighlighter
+      language={data.syntax}
+      style={atelierCaveDark}
+      showLineNumbers
+      customStyle={{
+        marginTop: -42,
+      }}
+    >
+      {data.sample}
+    </SyntaxHighlighter>
+  </div>
+);
 
 export default CodeHighlight;
