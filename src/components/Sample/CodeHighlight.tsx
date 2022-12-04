fd517c2333325e49d9c97900d6e73ca7e56978da
Burak <burak@90pixel.com>
2022-12-04 13:16:43 +0300

    Remove unnucessary return.

--- src/components/Sample/CodeHighlight.tsx
+++ src/components/Sample/CodeHighlight.tsx
@@ -4,28 +4,26 @@ import { atelierCaveDark } from "react-syntax-highlighter/dist/esm/styles/hljs";
 import Icon from "src/components/Icon";
 import Button from "../Button";
 
-const CodeHighlight = ({ data, onCopyCode }) => {
-  return (
-    <div className="group relative">
-      <Button onClick={onCopyCode} className="sticky left-full top-2">
-        <Icon
-          size={24}
-          icon="copy"
-          className="mr-1 text-black opacity-0 transition-all group-hover:opacity-70 dark:text-white"
-        />
-      </Button>
-      <SyntaxHighlighter
-        language={data.syntax}
-        style={atelierCaveDark}
-        showLineNumbers
-        customStyle={{
-          marginTop: -42,
-        }}
-      >
-        {data.sample}
-      </SyntaxHighlighter>
-    </div>
-  );
-};
+const CodeHighlight = ({ data, onCopyCode }) => (
+  <div className="group relative">
+    <Button onClick={onCopyCode} className="sticky left-full top-2">
+      <Icon
+        size={24}
+        icon="copy"
+        className="mr-1 text-black opacity-0 transition-all group-hover:opacity-70 dark:text-white"
+      />
+    </Button>
+    <SyntaxHighlighter
+      language={data.syntax}
+      style={atelierCaveDark}
+      showLineNumbers
+      customStyle={{
+        marginTop: -42,
+      }}
+    >
+      {data.sample}
+    </SyntaxHighlighter>
+  </div>
+);
 
 export default CodeHighlight;
