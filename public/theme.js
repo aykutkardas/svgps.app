4bd6477479532387bd59f953929ee9aad56e316d
Burak <burak@90pixel.com>
2022-12-07 13:52:06 +0300

    Remove unnecessary theme file.

--- public/theme.js
+++
@@ -1,6 +0,0 @@
-const localTheme= localStorage.getItem('theme');
-const themeValue = JSON.parse(localTheme);
-
-if(themeValue){
-  document.querySelector('html').dataset.theme=themeValue.value;
-}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,6 @@ import "nprogress/nprogress.css";
 
 import { AppProps } from "next/app";
 import Router from "next/router";
-import Script from "next/script";
 import NProgress from "nprogress";
 import { Toaster } from "react-hot-toast";
 
@@ -22,7 +21,6 @@ const MyApp = ({ Component, pageProps }: AppProps) => (
           "bg-neutral-200 dark:bg-neutral-800 text-neutral-900 dark:text-neutral-50",
       }}
     />
-    <Script src="/theme.js" defer={false} strategy="beforeInteractive" />
     <Component {...pageProps} />
   </>
 );
