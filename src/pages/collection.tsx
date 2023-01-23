243a6d436b558c363b0ee43dac6430eb52ab7ad5
Aykut Kardaş <aykutkrds@gmail.com>
2023-01-23 19:23:31 +0300

    Update UI with minor changes (#274)

--- src/pages/collection.tsx
+++ src/pages/collection.tsx
@@ -6,7 +6,7 @@ import { DragDropProvider } from "src/context/DragDropContext";
 import IconSetPreview from "src/components/IconSetPreview";
 
 const AppPage = () => (
-  <div className="mx-auto flex max-h-screen w-full flex-col p-3">
+  <div className="mx-auto flex max-h-screen w-full flex-col py-3 px-3 md:px-8">
     <Head>
       <title>SVGPS - Create your own icon collection</title>
     </Head>
