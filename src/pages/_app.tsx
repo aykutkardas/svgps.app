0ea1ea9786d7e0b71310fd28419d79069c3e3c29
Aykut Kardaş <aykutkrds@gmail.com>
2023-01-25 21:52:17 +0300

    Update toast style (#281)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -17,9 +17,25 @@ const MyApp = ({ Component, pageProps }: AppProps) => (
     <Toaster
       position="top-center"
       toastOptions={{
-        duration: 1000,
+        duration: 1500,
         className:
-          "bg-neutral-200 dark:bg-neutral-800 text-neutral-900 dark:text-neutral-50",
+          "bg-neutral-200 dark:bg-neutral-800 text-xs text-neutral-900 dark:text-neutral-50 border border-neutral-200/10",
+        style: {
+          padding: "5px 10px",
+          color: "white",
+        },
+        success: {
+          iconTheme: {
+            primary: "rgb(192,132,252)",
+            secondary: "white",
+          },
+        },
+        error: {
+          iconTheme: {
+            primary: "rgb(244,63,94)",
+            secondary: "white",
+          },
+        },
       }}
     />
     <Component {...pageProps} />
