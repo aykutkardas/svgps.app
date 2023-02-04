4027644112bc95ceff00fa2f619ed51d37c50aa1
Aykut Kardaş <aykutkrds@gmail.com>
2023-02-04 15:37:35 +0300

    Update collection page

--- src/pages/collection/index.tsx
+++ src/pages/collection/index.tsx
@@ -1,16 +1,16 @@
+import { useEffect, useRef } from "react";
 import Head from "next/head";
 import ContentLoader from "react-content-loader";
 import { useRouter } from "next/router";
+import clsx from "clsx";
 
 import Header from "src/components/Header";
-import { IconsProvider } from "src/context/IconsContext";
-import { DragDropProvider } from "src/context/DragDropContext";
 import IconSetPreview from "src/components/IconSetPreview";
-import { useEffect, useRef, useState } from "react";
-import { useAuthContext } from "../../context/AuthContext";
 import CollectionCard from "src/components/CollectionCard";
 import Icon from "src/components/Icon";
-
+import { useAuthContext } from "src/context/AuthContext";
+import { IconsProvider } from "src/context/IconsContext";
+import { DragDropProvider } from "src/context/DragDropContext";
 import { createCollection } from "src/api/collection";
 
 const CollectionPage = () => {
@@ -49,23 +49,31 @@ const CollectionPage = () => {
   }, []);
 
   return (
-    <div className="container mx-auto flex h-screen flex-col p-3">
+    <div
+      className={clsx(
+        "mx-auto flex h-screen flex-col p-3",
+        auth ? "container" : "w-full"
+      )}
+    >
       <Head>
         <title>SVGPS - Create your own icon collection</title>
       </Head>
       <Header />
-      <IconsProvider>
-        <DragDropProvider>
-          <div className="py-3">{!auth && <IconSetPreview isCollection />}</div>
-        </DragDropProvider>
-      </IconsProvider>
-      <div
-        ref={cardsRef}
-        id="cards"
-        className="mt-[300px] flex w-full flex-wrap items-center justify-center md:items-start md:justify-start"
-      >
-        {auth &&
-          (loading
+      {!auth ? (
+        <IconsProvider>
+          <DragDropProvider>
+            <div className="py-3">
+              {!auth && <IconSetPreview isCollection />}
+            </div>
+          </DragDropProvider>
+        </IconsProvider>
+      ) : (
+        <div
+          ref={cardsRef}
+          id="cards"
+          className="mt-[200px] flex w-full flex-wrap items-center justify-center md:items-start md:justify-start"
+        >
+          {loading
             ? [1, 2, 3].map((i) => (
                 <ContentLoader
                   viewBox="0 0 320 160"
@@ -86,21 +94,22 @@ const CollectionPage = () => {
                   userAvatars={[auth.profilePicture]}
                   count={collection.icons?.split('"properties":').length - 1}
                 />
-              )))}
-        <div
-          className="m-[10px] h-40 w-80 cursor-pointer select-none overflow-hidden p-[1px]"
-          onClick={newCollection}
-        >
-          <div className="h-40 rounded-lg border-2 border-dashed border-neutral-700 bg-neutral-800/20 text-neutral-500 transition-colors hover:border-neutral-600 hover:text-neutral-400">
-            <div className="flex h-full w-full flex-col items-center justify-center">
-              <Icon icon="close" className="mb-3 rotate-45" size={30}></Icon>
-              <h2 className="mx-auto text-base font-medium">
-                Create New Collection
-              </h2>
+              ))}
+          <div
+            className="m-[10px] h-40 w-80 cursor-pointer select-none overflow-hidden p-[1px]"
+            onClick={newCollection}
+          >
+            <div className="h-40 rounded-lg border-2 border-dashed border-neutral-700 bg-neutral-800/20 text-neutral-500 transition-colors hover:border-neutral-600 hover:text-neutral-400">
+              <div className="flex h-full w-full flex-col items-center justify-center">
+                <Icon icon="close" className="mb-3 rotate-45" size={30}></Icon>
+                <h2 className="mx-auto text-base font-medium">
+                  Create New Collection
+                </h2>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
