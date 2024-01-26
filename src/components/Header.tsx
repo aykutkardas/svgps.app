91b05202214471b799d1ec26e4cfe2ebef2508f9
Aykut Kardaş <aykutkrds@gmail.com>
2024-01-26 23:29:51 +0300

    feat: add warn and update sponsorship status

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -26,45 +26,63 @@ const Header = () => {
   }, [router.query]);
 
   return (
-    <div className="flex h-12 w-full shrink-0 justify-between">
-      <div className="flex cursor-pointer select-none flex-nowrap items-center justify-center text-violet-600 dark:text-violet-500">
-        <Link href="/">
-          <span className="flex items-center">
-            <Icon icon="package" className="h-8 w-8" />
-            <span className="hidden font-bold dark:after:text-neutral-400 sm:block">
-              SVGPS
-            </span>
-          </span>
-        </Link>
+    <>
+      <div className="absolute left-0 top-0 flex h-20 w-screen items-center justify-center bg-amber-500/50 pl-1 text-center text-sm text-white shadow-md md:h-14 lg:h-9">
+        <p className="">
+          Our open-source project is temporarily unavailable, and we need your
+          support to bring it back faster.{" "}
+          <a
+            href="https://github.com/sponsors/aykutkardas"
+            className="inline-flex underline"
+            target="_blank"
+          >
+            Sponsor us now!
+          </a>
+        </p>
       </div>
-      <nav className="flex items-center gap-3  text-sm font-medium sm:gap-5">
-        <NavLink href="/store">Store</NavLink>
-        <NavLink href="/collection">Collection</NavLink>
-        <a
-          href="https://github.com/aykutkardas/svgps.app"
-          target="_blank"
-          rel="noreferrer"
-          aria-label="GitHub"
-          className="flex items-center text-neutral-700 hover:text-neutral-500 dark:text-neutral-100 dark:hover:text-neutral-300"
-        >
-          <Icon icon="github" className="h-5 w-5" />
-        </a>
-        {/* notifications are temporarily hidden */}
-        {false && <Notification />}
-        {!auth && (
-          <>
-            <Button onClick={() => setOpenLoginDialog(true)} variant="primary">
-              Login
-            </Button>
-            <LogInDialog
-              isOpen={openLoginDialog}
-              setIsOpen={setOpenLoginDialog}
-            />
-          </>
-        )}
-        {auth && <UserMenu auth={auth} />}
-      </nav>
-    </div>
+      <div className="relative mt-20 flex h-12 w-full shrink-0 justify-between md:mt-16 lg:mt-10">
+        <div className="flex cursor-pointer select-none flex-nowrap items-center justify-center text-violet-600 dark:text-violet-500">
+          <Link href="/">
+            <span className="flex items-center">
+              <Icon icon="package" className="h-8 w-8" />
+              <span className="hidden font-bold dark:after:text-neutral-400 sm:block">
+                SVGPS
+              </span>
+            </span>
+          </Link>
+        </div>
+        <nav className="flex items-center gap-3  text-sm font-medium sm:gap-5">
+          <NavLink href="/store">Store</NavLink>
+          <NavLink href="/collection">Collection</NavLink>
+          <a
+            href="https://github.com/aykutkardas/svgps.app"
+            target="_blank"
+            rel="noreferrer"
+            aria-label="GitHub"
+            className="flex items-center text-neutral-700 hover:text-neutral-500 dark:text-neutral-100 dark:hover:text-neutral-300"
+          >
+            <Icon icon="github" className="h-5 w-5" />
+          </a>
+          {/* notifications are temporarily hidden */}
+          {false && <Notification />}
+          {!auth && (
+            <>
+              <Button
+                onClick={() => setOpenLoginDialog(true)}
+                variant="primary"
+              >
+                Login
+              </Button>
+              <LogInDialog
+                isOpen={openLoginDialog}
+                setIsOpen={setOpenLoginDialog}
+              />
+            </>
+          )}
+          {auth && <UserMenu auth={auth} />}
+        </nav>
+      </div>
+    </>
   );
 };
 
