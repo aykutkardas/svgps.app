88b7e8274a9485f2862a6bb829b1c0085a83b9c6
Aykut Kardaş <aykutkrds@gmail.com>
2024-02-18 22:28:13 +0300

    fix: update eslint and next config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
-module.exports = {
-  trailingSlash: true,
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    // https://nextjs.org/docs/messages/missing-suspense-with-csr-bailout
+    missingSuspenseWithCSRBailout: false,
+  },
+}
+
+module.exports = nextConfig
