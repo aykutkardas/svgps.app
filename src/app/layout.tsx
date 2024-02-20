23584ab66e3ac4c425cfb07cfc718488e37162be
Aykut Kardaş <aykutkrds@gmail.com>
2024-02-20 19:51:06 +0300

    fix: script usage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,11 +3,15 @@ import { Metadata } from "next";
 import "src/index.css";
 import "nprogress/nprogress.css";
 import ToastWrapper from "src/components/ToastWrapper";
+import { Inter } from "next/font/google";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "SVGPS - Your Icon Store and Storage",
   description: "Welcome to Next.js",
 };
+
+const inter = Inter({ subsets: ["latin"], weight: ["400", "500", "700"] });
 
 export default function RootLayout({
   // Layouts must accept a children prop.
@@ -18,7 +22,37 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" data-theme="dark">
-      <body>
+      <head>
+        {/* <!-- Google Analytics --> */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-CHZ8XNEG79"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag() { dataLayer.push(arguments) }
+                  gtag("js", new Date());
+                  gtag("config", "G-CHZ8XNEG79");
+                  `,
+          }}
+        />
+        {/* <!-- End Google Analytics --> */}
+
+        <Script
+          id="theme-initializer"
+          strategy="beforeInteractive"
+          defer={false}
+          dangerouslySetInnerHTML={{
+            __html: `
+      document.querySelector("html").dataset.theme = "dark";`,
+          }}
+        />
+
+        <link rel="shortcut icon" href="/favicon.svg" />
+      </head>
+      <body className={inter.className}>
         {children}
         <ToastWrapper />
       </body>
