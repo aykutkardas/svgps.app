0b13d750350a460af6bf11e8334434e4fca5e74f
elinoza <hillcakmak@gmail.com>
2024-03-10 23:03:23 +0300

    scrollbar color change acc. to dark mode
    -update on the tailwind.config.js
    -"The selector strategy replaced the class strategy in Tailwind CSS v3.4.1."

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.tsx"],
-  darkMode: ["class", "[data-theme='dark']"],
+  darkMode: ["selector", "[data-theme='dark']"],
   theme: {
     fontFamily: {
       roboto: ["Roboto"],
